dac46dc802b3f838720b9f20f3723606d3602efe
yasser9947 <Y.AlMuhtarish@Takamol.sa>
2025-04-15 10:49:13 +0300

    add product services

--- product-service/src/test/java/com/example/productservice/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/example/productservice/ProductServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.microservices;
+package com.example.productservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MicroServicesApplicationTests {
+class ProductServiceApplicationTests {
 
     @Test
     void contextLoads() {
